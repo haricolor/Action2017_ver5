04674d70d0fe9d1b5efb2034adfe03dfd4714e47
DESKTOP-T3AH2F7\tabito <tabito.dqlove@gmail.com>
2017-07-20 13:35:59 +0900

    スクリプトの更新

--- SuicaProject/Assets/Player.cs
+++ SuicaProject/Assets/Player.cs
@@ -4,29 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    SpriteRenderer MainSpriteRenderer;
-    // publicで宣言し、inspectorで設定可能にする
-    public Sprite StandbySprite;
-    public Sprite HoldSprite;
-    public Sprite SlashSprite;
-
-    void Start()
-    {
-        // このobjectのSpriteRendererを取得
-        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-    }
-
-    // 何かしらのタイミングで呼ばれる
-    void ChangeStateToHold()
-    {
-        // SpriteRenderのspriteを設定済みの他のspriteに変更
-        // 例) HoldSpriteに変更
-        MainSpriteRenderer.sprite = HoldSprite;
-    }
-
-
-
-    /*public bool isGround;*/
     public float speed = 5;
 
     void Update()
--- SuicaProject/Assets/PlayerWeapon.cs
+++ SuicaProject/Assets/PlayerWeapon.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour {
+    void Update()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Set_position();
+        Attack();
+
+    }
+
+    public void Set_position()
+    {
+
+        Vector3 PlayerPosition = GameObject.Find("Player").transform.position; //プレイヤーのポジションを取得
+        Vector3 PlayerScale = GameObject.Find("Player").transform.localScale; //プレイヤーのスケールを取得
+        Vector3 WeaponScale = transform.localScale; //武器のスケールを取得
+
+        if (PlayerScale.x < 0)
+        {
+            WeaponScale.x = -2; //武器を左向きに
+
+            //プレイヤーに合わせて武器も移動
+            GameObject.Find("PlayerWeapon").transform.position = new Vector2(PlayerPosition.x - 1, PlayerPosition.y);
+        }
+        if (PlayerScale.x > 0)
+        {
+            WeaponScale.x = 2; //武器を右向きに
+
+            //プレイヤーに合わせて武器も移動
+            GameObject.Find("PlayerWeapon").transform.position = new Vector2(PlayerPosition.x + 1, PlayerPosition.y);
+        }
+        transform.localScale = WeaponScale;
+    }
+
+    public void Attack()
+    {
+
+        //アニメーションを再生
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
 }

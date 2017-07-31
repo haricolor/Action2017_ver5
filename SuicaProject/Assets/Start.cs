03f62fe5d07917dfcc373900bc8d7d7d7c770abd
大江真也 <o.shinya126@gmail.com>
2017-07-31 18:23:06 +0900

    no message

---
+++ SuicaProject/Assets/Start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+
+	public void OnClick()
+	{
+		Debug.Log("Button click!");
+		// 非表示にする
+		gameObject.SetActive(false);
+	}
+
+/*public class SceneLoadScript : MonoBehaviour
+{
+	public void SceneLoad()
+	{
+		Application.LoadLevel("main");
+	}
+}*/
+}
